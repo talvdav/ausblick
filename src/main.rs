39ca9e6d71e77ebd36bd2eb53eca61029a3504e3
Thomas A. Alvarado Davalos <talvdav@mailbox.org>
2022-04-26 12:10:59 +0200

    added open file dialog

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
+use native_dialog::FileDialog;
 use std::env;
 
-use iced::
-{
-    button, Button, //Align,  ProgressBar, Radio, Row, Container, Space, HorizontalAlignment,
-    scrollable, Column, Scrollable, Element,
-    Rule, Sandbox, Settings, Text, Length, 
+use iced::{
+    button,
+    scrollable,
+    Button, //Align,  ProgressBar, Radio, Row, Container, Space, HorizontalAlignment,
+    Column,
+    Element,
+    Length,
+    Sandbox,
+    Scrollable,
+    Settings,
+    Text,
 };
 
-use msg_parser::
-{
-    Outlook,
-    // Person, Attachment, TransportHeaders
-};
+use msg_parser::Outlook;
 
 fn main() -> iced::Result {
     Ausblick::run(Settings::default())
@@ -33,46 +36,53 @@ impl Sandbox for Ausblick {
     type Message = Message;
 
     fn new() -> Self {
-
-
         let argc = env::args().count();
 
-        if argc >= 2
-        {
+        if argc >= 2 {
             let args: Vec<String> = env::args().collect();
             let path: String = args[1].to_string();
             let outlook = Outlook::from_path(path).unwrap();
-            Ausblick
-            {
+            Ausblick {
                 scrollable_state: scrollable::State::new(),
                 subject: outlook.subject,
                 body: outlook.body,
-		        open_button: button::State::new(),
+                open_button: button::State::new(),
             }
-        }
-	    else
-	    {
-            Ausblick
-            {
+        } else {
+            Ausblick {
                 scrollable_state: scrollable::State::new(),
                 subject: "".to_string(),
                 body: "".to_string(),
-            	open_button: button::State::new(),
+                open_button: button::State::new(),
             }
-	    }
-    }
-
-    fn title (&self) -> String {
-        String::from("Ausblick - Simple .msg Viewer")
-    }
-
-    fn update (&mut self, message: Message) {
-        match message {
-	    Message::OpenFileDialog => {}
         }
     }
 
-    fn view (&mut self) -> Element<Message> {
+    fn title(&self) -> String {
+        String::from("Ausblick - Simple .msg Viewer")
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::OpenFileDialog => {
+                let path = FileDialog::new()
+                    .set_location("~")
+                    .show_open_single_file()
+                    .unwrap();
+
+                match path {
+                    Some(path) => {
+                        let outlook = Outlook::from_path(path).unwrap();
+                        self.subject = outlook.subject;
+                        self.body = outlook.body;
+                    }
+                    None => return,
+                };
+            }
+        }
+    }
+
+    fn view(&mut self) -> Element<Message> {
         Column::new()
             .push(
                 Scrollable::new(&mut self.scrollable_state)
@@ -82,11 +92,14 @@ impl Sandbox for Ausblick {
                     .push(Text::new("Message: ").size(30))
                     .push(Text::new(self.body.clone()))
                     .width(Length::Fill)
-                    .height(Length::Fill))
-	    .push(Button::new(&mut self.open_button, Text::new("Open"))
-		  .width(Length::Fill)
-		  .padding(20)
-		  .on_press(Message::OpenFileDialog))
+                    .height(Length::Fill),
+            )
+            .push(
+                Button::new(&mut self.open_button, Text::new("Open"))
+                    .width(Length::Fill)
+                    .padding(20)
+                    .on_press(Message::OpenFileDialog),
+            )
             .into()
     }
 }
