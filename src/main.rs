8668033ef079ce7509b2af22f9aae8c919114a92
Thomas A. Alvarado Davalos <thomas.alvarado@mailbox.org>
2023-03-20 14:41:14 +0100

    updated dependencies

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@ use native_dialog::FileDialog;
 use std::env;
 
 use iced::{
-    button, scrollable, Button, Column, Element, Length, Sandbox, Scrollable, Settings, Text,
+    Element, Sandbox,  Settings, alignment,
+};
+
+use iced::widget::{
+    button, Column, text, scrollable
 };
 
 use msg_parser::Outlook;
@@ -12,10 +16,8 @@ fn main() -> iced::Result {
 }
 
 struct Ausblick {
-    scrollable_state: scrollable::State,
     subject: String,
     body: String,
-    open_button: button::State,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -34,17 +36,13 @@ impl Sandbox for Ausblick {
             let path: String = args[1].to_string();
             let outlook = Outlook::from_path(path).unwrap();
             Ausblick {
-                scrollable_state: scrollable::State::new(),
                 subject: outlook.subject,
                 body: outlook.body,
-                open_button: button::State::new(),
             }
         } else {
             Ausblick {
-                scrollable_state: scrollable::State::new(),
                 subject: "".to_string(),
                 body: "".to_string(),
-                open_button: button::State::new(),
             }
         }
     }
@@ -74,24 +72,20 @@ impl Sandbox for Ausblick {
         }
     }
 
-    fn view(&mut self) -> Element<Message> {
-        Column::new()
-            .push(
-                Scrollable::new(&mut self.scrollable_state)
-                    .padding(20)
-                    .push(Text::new("Subject: ").size(30))
-                    .push(Text::new(self.subject.clone()))
-                    .push(Text::new("Message: ").size(30))
-                    .push(Text::new(self.body.clone()))
-                    .width(Length::Fill)
-                    .height(Length::Fill),
-            )
-            .push(
-                Button::new(&mut self.open_button, Text::new("Open"))
-                    .width(Length::Fill)
-                    .padding(20)
-                    .on_press(Message::OpenFileDialog),
-            )
-            .into()
+    fn view(&self) -> Element<Message> {
+        let scrollable_content: Element<Message> =
+            Element::from(
+                scrollable(
+                    Column::new()
+                        .push(button(
+                            text("open").horizontal_alignment(alignment::Horizontal::Center))
+                              .on_press(Message::OpenFileDialog))
+                        .push(text("Subject:"))
+                        .push(text(&self.subject))
+                        .push(text("Messge:"))
+                        .push(text(&self.body))
+                ));
+        scrollable_content
     }
+
 }
