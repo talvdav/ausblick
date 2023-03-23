fd6c5aeecdc9d4d6d1744bc9e75bfdd4c56567cc
Thomas A. Alvarado Davalos <talvdav@mailbox.org>
2023-03-23 23:13:03 +0100

    progress

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
 use native_dialog::FileDialog;
 
 use std::env;
+use std::ffi::OsStr;
 use std::path::Path;
 use std::path::PathBuf;
-use std::ffi::OsStr;
 
-use iced::{
-    Element, Sandbox,  Settings, alignment,
-};
+use iced::{alignment, Element, Sandbox, Settings};
 
-use iced::widget::{
-    button, Column, text, scrollable,
-};
+use iced::widget::{button, scrollable, text, Column};
 
 use msg_parser::Outlook;
 
@@ -25,15 +21,26 @@ struct Ausblick {
 }
 
 fn parse_mailformats(path: &str) -> Ausblick {
-
     let file = Path::new(path);
     let ext = file.extension().unwrap();
 
-    Ausblick {
-        subject: "Foo".to_string(),
-        body: "Bar".to_string(),
+    if ext == PathBuf::from("msg") {
+        return Ausblick {
+            subject: "MSG".to_string(),
+            body: "MSG".to_string(),
+        };
     }
-
+    if ext == PathBuf::from("EML") {
+        return Ausblick {
+            subject: "EML".to_string(),
+            body: "EML".to_string(),
+        };
+    } else {
+        Ausblick {
+            subject: "Foo".to_string(),
+            body: "Bar".to_string(),
+        }
+    }
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -79,8 +86,8 @@ impl Sandbox for Ausblick {
 
                 match path {
                     Some(path) => {
-                        let p = &path.into_os_string().into_string().unwrap(); 
-                        println!("P {}",&p);
+                        let p = &path.into_os_string().into_string().unwrap();
+                        println!("P {}", &p);
                         let f = parse_mailformats(&p);
                         self.subject = f.subject;
                         self.body = f.body;
@@ -92,17 +99,17 @@ impl Sandbox for Ausblick {
     }
 
     fn view(&self) -> Element<Message> {
-        let scrollable_content: Element<Message> =
-            Element::from(
-                scrollable(
-                    Column::new()
-                        .push(button(
-                            text("open").horizontal_alignment(alignment::Horizontal::Center))
-                              .on_press(Message::OpenFileDialog))
-                        .push(text("Subject:").size(28))
-                        .push(text(&self.subject))
-                        .push(text("Messge:").size(28))
-                        .push(text(&self.body))));
+        let scrollable_content: Element<Message> = Element::from(scrollable(
+            Column::new()
+                .push(
+                    button(text("open").horizontal_alignment(alignment::Horizontal::Center))
+                        .on_press(Message::OpenFileDialog),
+                )
+                .push(text("Subject:").size(28))
+                .push(text(&self.subject))
+                .push(text("Messge:").size(28))
+                .push(text(&self.body)),
+        ));
         scrollable_content
     }
 }
